42630b79d837d45c2e5bad8a0131e01247823fad
george-matheww <gpegasus007@gmail.com>
2023-04-15 12:57:29 +0530

    sim

---
+++ slam_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/pradeepkumar/slam_ws/src
+# Build directory: /home/pradeepkumar/slam_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("explorer_bot")
---
+++ slam_ws/build/explorer_bot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pradeepkumar/slam_ws/src/explorer_bot
+# Build directory: /home/pradeepkumar/slam_ws/build/explorer_bot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
